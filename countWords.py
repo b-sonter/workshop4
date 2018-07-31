db95df4a3242ffc60c315c7b61f575e7abf81903
b-sonter <40931305+b-sonter@users.noreply.github.com>
2018-07-31 12:43:36 +1000

    Read in mbox-short

--- countWords.py
+++ countWords.py
@@ -1,14 +1,15 @@
 import sys
 
-dropChars = "!@#$%^& ()_+-={}[]|\\:;\"'<>,.?/1234567890"
-dropDict = dict([(c, '') for c in dropChars])
-dropTable = str.maketrans(dropDict)
+with open("mbox-short.txt","r") as file:
+    dropChars = "!@#$%^&()_+-={}[]|\\:;\"'<>,.?/1234567890"
+    dropDict = dict([(c, '') for c in dropChars])
+    dropTable = str.maketrans(dropDict)
 
-freq = {}
+    freq = {}
 
-for word in sys.stdin.read().split():
-    tword = word.upper().translate(dropTable)
-    freq[tword] = freq.get(tword,0)+1
+    for word in file.read().split():
+        tword = word.upper().translate(dropTable)
+        freq[tword] = freq.get(tword,0)+1
 
-for key in freq.keys():
-    print(key, freq[key])
+    for key in freq.keys():
+        print(key, freq[key])
