7209e12d2f4f57e20ac43508a212cbe24e52df9f
b-sonter <40931305+b-sonter@users.noreply.github.com>
2018-07-31 13:07:48 +1000

    Revert "Read in mbox-short"
    This reverts commit db95df4a3242ffc60c315c7b61f575e7abf81903.

--- countWords.py
+++ countWords.py
@@ -1,15 +1,14 @@
 import sys
 
-with open("mbox-short.txt","r") as file:
-    dropChars = "!@#$%^&()_+-={}[]|\\:;\"'<>,.?/1234567890"
-    dropDict = dict([(c, '') for c in dropChars])
-    dropTable = str.maketrans(dropDict)
+dropChars = "!@#$%^& ()_+-={}[]|\\:;\"'<>,.?/1234567890"
+dropDict = dict([(c, '') for c in dropChars])
+dropTable = str.maketrans(dropDict)
 
-    freq = {}
+freq = {}
 
-    for word in file.read().split():
-        tword = word.upper().translate(dropTable)
-        freq[tword] = freq.get(tword,0)+1
+for word in sys.stdin.read().split():
+    tword = word.upper().translate(dropTable)
+    freq[tword] = freq.get(tword,0)+1
 
-    for key in freq.keys():
-        print(key, freq[key])
+for key in freq.keys():
+    print(key, freq[key])
